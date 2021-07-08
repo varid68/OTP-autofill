bd8baf041a2ac1b0da07ed20606cb4ca28fa4478
kakashi <kakashi@kakashis-MacBook-Pro.local>
2021-07-08 17:38:33 +0700

    Slicing login page

--- src/screens/Login/LoginStyles.js
+++ src/screens/Login/LoginStyles.js
@@ -1,6 +1,48 @@
 import { StyleSheet } from 'react-native'
+import { GRAY, GRAY_25, GHOST, YELLOW } from 'constants/Colors'
+import { ROW_CENTER } from 'constants/Styles'
 
 
 export default StyleSheet.create({
+	container: {
+		backgroundColor: GRAY,
+		flex: 1,
+		padding: 20
+	},
 
+	clearText: {
+		position: 'absolute',
+		zIndex: 20,
+		right: 10,
+		padding: 10
+	},
+	countryCode: {
+		...ROW_CENTER,
+		padding: 15,
+		backgroundColor: GRAY_25
+	},
+	countryFlag: {
+		resizeMode: 'contain',
+		width: 25,
+		height: 25,
+		borderRadius: 20
+	},
+	field: {
+		backgroundColor: GRAY_25,
+		flex: 1,
+		color: GHOST,
+		fontSize: 20,
+	},
+
+	btnSubmit: {
+		...ROW_CENTER,
+		position: 'absolute',
+		bottom: 20,
+		right: 20,
+		width: 50,
+		height: 50,
+		borderRadius: 3,
+		backgroundColor: YELLOW,
+		padding: 10
+	}
 })
--- src/screens/Login/LoginView.js
+++ src/screens/Login/LoginView.js
@@ -1,7 +1,11 @@
 import React, { useContext } from 'react'
-import { View } from 'react-native'
+import { View, StatusBar, TextInput, Image, TouchableOpacity } from 'react-native'
 import Text from 'components/Text'
-import SafeAreaView from 'react-native-safe-area-context'
+import Icon from 'react-native-vector-icons/MaterialIcons'
+import Ionicons from 'react-native-vector-icons/Ionicons'
+import { GHOST, GRAY, GRAY_25, GRAY_50, GRAY_75, MERCURY, YELLOW } from 'constants/Colors'
+import { LIST_ITEM_BASE } from 'constants/Styles'
+import styles from './LoginStyles'
 
 import { LoginContext } from './LoginContext'
 
@@ -10,7 +14,79 @@ const LoginView = ({ navigation }) => {
   const value = useContext(LoginContext)
 
   return (
-    <Text>INI PAGE Login</Text>
+    <View style={styles.container}>
+      <StatusBar backgroundColor={GRAY} />
+
+      <View style={{
+        ...LIST_ITEM_BASE,
+        marginBottom: 20
+      }}>
+        <Icon name='clear' size={30} color={MERCURY} />
+        <Text color={YELLOW}>Register</Text>
+      </View>
+
+      <Text color={GHOST} size='huge' weight={500}>Log In</Text>
+
+      <View style={{
+        flexDirection: 'column',
+        marginTop: 20
+      }}>
+        <View style={LIST_ITEM_BASE}>
+          <Text color={MERCURY} size='tiny'>Mobile</Text>
+          <Text color={YELLOW} size='tiny'>Log in with email</Text>
+        </View>
+        <View style={{
+          flexDirection: 'row',
+          marginTop: 10
+        }}>
+          <TouchableOpacity
+            onPress={null}
+            style={[styles.clearText, { top: 7 }]}>
+            <Icon name='close' color={GRAY_75} size={20} />
+          </TouchableOpacity>
+          <View style={styles.countryCode}>
+            <Image style={styles.countryFlag} source={require('images/Flag-Indonesia.jpeg')} />
+            <Text color={GRAY_75} style={{ paddingHorizontal: 7 }}>+62</Text>
+            <Ionicons name='caret-down' color={GHOST} size={18} />
+          </View>
+          <TextInput
+            keyboardType='number-pad'
+            placeholder='Mobile Number'
+            placeholderTextColor={GRAY_50}
+            style={styles.field} />
+        </View>
+      </View>
+
+
+      <View style={{
+        flexDirection: 'column',
+        marginTop: 20
+      }}>
+        <Text color={MERCURY} size='tiny'>Password</Text>
+        <View style={{
+          flexDirection: 'row',
+          marginTop: 10
+        }}>
+          <TouchableOpacity
+            onPress={null}
+            style={[styles.clearText, { top: 15 }]}>
+            <Ionicons name='eye-off-outline' color={GRAY_75} size={20} />
+          </TouchableOpacity>
+          <TextInput
+            placeholder='Please enter password'
+            placeholderTextColor={GRAY_50}
+            style={[styles.field, { paddingLeft: 20, marginVertical: 10 }]} />
+        </View>
+        <Text color={MERCURY} size='tiny'>Password must be at least 8 characters with uppercase letters.</Text>
+      </View>
+
+
+      <Text size='tiny' color={YELLOW} style={{ marginTop: 40 }}>Forgot password?</Text>
+
+      <TouchableOpacity style={styles.btnSubmit}>
+        <Ionicons name='arrow-forward' size={30} />
+      </TouchableOpacity>
+    </View>
   )
 }
 
